8939d729ce4fd1b4ee4b3b578a1f2dede4962615
udoyen <datameshprojects@gmail.com>
2020-12-15 21:02:37 +0100

    Subtitle added but not tested

--- FlaskWebProject/forms.py
+++ FlaskWebProject/forms.py
@@ -5,14 +5,17 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextA
 from wtforms.validators import DataRequired
 from wtforms.widgets import TextArea
 
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
+
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
+    subtitle = StringField('Subtitle', validators=[DataRequired])
     author = StringField('Author', validators=[DataRequired()])
     body = TextAreaField('Body', validators=[DataRequired()])
     image_path = FileField('Image', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -10,8 +10,10 @@ from flask import flash
 blob_container = app.config['BLOB_CONTAINER']
 blob_service = BlockBlobService(account_name=app.config['BLOB_ACCOUNT'], account_key=app.config['BLOB_STORAGE_KEY'])
 
+
 def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -28,14 +30,17 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
 class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150))
+    subtitle = db.Column(db.String(150))
     author = db.Column(db.String(75))
     body = db.Column(db.String(800))
     image_path = db.Column(db.String(100))
@@ -47,22 +52,23 @@ class Post(db.Model):
 
     def save_changes(self, form, file, userId, new=False):
         self.title = form.title.data
+        self.subtitle = form.subtitle.data
         self.author = form.author.data
         self.body = form.body.data
         self.user_id = userId
 
         if file:
             filename = secure_filename(file.filename);
-            fileextension = filename.rsplit('.',1)[1];
+            fileextension = filename.rsplit('.', 1)[1];
             Randomfilename = id_generator();
             filename = Randomfilename + '.' + fileextension;
             try:
                 blob_service.create_blob_from_stream(blob_container, filename, file)
-                if(self.image_path):
+                if (self.image_path):
                     blob_service.delete_blob(blob_container, self.image_path)
             except Exception:
                 flash(Exception)
-            self.image_path =  filename
+            self.image_path = filename
         if new:
             db.session.add(self)
         db.session.commit()
